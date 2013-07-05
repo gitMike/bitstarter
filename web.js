2d772c6ed0c018d0c24cd2b9fd4938e59e028c0d
John Smith <example@standford.edu>
2013-07-05 18:16:16 +0000

    Updated web.js - testing

--- web.js
+++ web.js
@@ -1,9 +1,12 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var mssg = new Buffer((fs.readFileSync('index.html') )).toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(mssg);
 });
 
 var port = process.env.PORT || 5000;
